2b926a2dd8a5e89b683dfcbe7ecfde26b840d643
hirosh7 <hirosh7@gmail.com>
2015-02-24 11:20:21 -0700

    stable master branch

--- app/src/main/java/com/frostycold/empublite/EmPubLiteActivity.java
+++ app/src/main/java/com/frostycold/empublite/EmPubLiteActivity.java
@@ -11,7 +11,7 @@ public class EmPubLiteActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main2);
+        setContentView(R.layout.main);
     }
 
 
